9d530d3a3e109ed9373c29f27d51063bc8506ffb
marshall <mwhatters@gmail.com>
2021-04-19 14:47:59 -0700

    fooling around

---
+++ src/globals.lua
@@ -0,0 +1,20 @@
+ audio = {
+   tracks = {
+     intro = 0
+   },
+   sounds = {
+     snare = 0,
+     kick = 1,
+     hat = 2,
+     drum_groove = 10
+   },
+ }
+
+buttons = {
+  left = 0,
+  right = 1,
+  up = 2,
+  down = 3,
+  z = 4,
+  x = 5,
+}
---
+++ src/player.lua
@@ -0,0 +1,39 @@
+player = {}
+player.x = 64
+player.y = 64
+player.height = 6
+player.width = 8
+player.speed_x = 3
+player.speed_y = 4
+player.spr = 0
+
+function player.update(self)
+  if btn(buttons.left) then
+    self.x -= self.speed_x
+    self.x = max(self.x, 0)
+  end
+  if btn(buttons.right) then
+    self.x += self.speed_x
+    self.x = min(self.x, 128 - self.width - 1)
+  end
+  if btn(buttons.up) then
+    self.y -= self.speed_y
+    self.y = max(self.y, 0)
+  end
+  if btn(buttons.down) then
+    self.y += self.speed_y
+    self.y = min(self.y, 129 - self.height - 1)
+  end
+end
+
+function player.draw(self)
+  -- position display
+  print("x:"..self.x, self.x - 10, self.y - 10, 15)
+  print("y:"..self.y, self.x - 10, self.y - 5, 13)
+
+  -- sprite
+  rectfill(self.x, self.y, self.x + self.width, self.y + self.height, 14)
+end
+
+-- function player.jump(self):
+-- end
